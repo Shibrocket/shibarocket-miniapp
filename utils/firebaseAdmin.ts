068c9bc1ddf1621085104430d8414c59924e0839
Shibrocket <shibarocket72@gmail.com>
2025-04-27 13:47:55 +0100

    fix: correct firebaseAdmin export db

--- utils/firebaseAdmin.ts
+++ utils/firebaseAdmin.ts
@@ -1,16 +1,17 @@
-import { initializeApp, cert, getApps } from "firebase-admin/app";
+// utils/firebaseAdmin.ts
+
+import { initializeApp, cert, getApps, getApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-export function initFirebaseAdmin() {
-  if (!getApps().length) {
-    initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      }),
-    });
-  }
-}
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+};
 
+const app = !getApps().length ? initializeApp({ credential: cert(serviceAccount) }) : getApp();
+
+const db = getFirestore(app);
+
+export { app, db };
 
