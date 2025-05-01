a69304d1f076b8e61895ccc95b8c359fdddb82bb
Shibrocket <shibarocket72@gmail.com>
2025-05-01 07:24:53 +0100

    Fix: Correct firebase-admin config key names

--- utils/firebaseAdmin.ts
+++ utils/firebaseAdmin.ts
@@ -3,12 +3,14 @@ import * as admin from "firebase-admin";
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     }),
   });
 }
 
 const db = admin.firestore();
 export { db };
+
+
