284d57e9687468dc68bf3780dda731ea13ba3cbc
Shibrocket <shibarocket72@gmail.com>
2025-05-02 23:52:02 +0100

    Add firebaseAdmin and update tsconfig for alias

--- utils/firebaseAdmin.ts
+++ utils/firebaseAdmin.ts
@@ -1,15 +1,16 @@
-m// utils/firebaseAdmin.ts
+// utils/firebaseAdmin.ts
 
 import * as admin from 'firebase-admin';
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY || '{}');
-
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    }),
   });
 }
 
 const db = admin.firestore();
-
 export { db };
