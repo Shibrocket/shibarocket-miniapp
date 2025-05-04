286b3718c7793cb04cff609154d0b485897d4007
Shibrocket <shibarocket72@gmail.com>
2025-05-04 19:33:58 +0100

    Added Admin Dashboard and Firebase integration

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgPHF_po0Xuq0E1hqJHK8GLnuxnDgTDbE",
+  authDomain: "shibarocket-tapapp-7538a-e890c.firebaseapp.com",
+  projectId: "shibarocket-tapapp-7538a-e890c",
+  storageBucket: "shibarocket-tapapp-7538a-e890c.firebasestorage.app",
+  messagingSenderId: "598699447123",
+  appId: "1:598699447123:web:a9dc39806df1d457ce07a6",
+  measurementId: "G-WCJ6BDXNSP"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db };
