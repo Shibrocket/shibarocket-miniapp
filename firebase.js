c854d9da191baa85ad145d532a8989beac268277
Shibrocket <shibarocket72@gmail.com>
2025-05-05 08:45:00 +0100

    Fix: use firebase.js correctly and remove firebaseConfig.ts

--- firebase.js
+++ firebase.js
@@ -1,18 +1,19 @@
 // firebase.js
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCgPHF_po0Xuq0E1hqJHK8GLnuxnDgTDbE",
   authDomain: "shibarocket-tapapp-7538a-e890c.firebaseapp.com",
   projectId: "shibarocket-tapapp-7538a-e890c",
-  storageBucket: "shibarocket-tapapp-7538a-e890c.firebasestorage.app",
+  storageBucket: "shibarocket-tapapp-7538a-e890c.appspot.com",
   messagingSenderId: "598699447123",
   appId: "1:598699447123:web:a9dc39806df1d457ce07a6",
   measurementId: "G-WCJ6BDXNSP"
 };
 
-const app = initializeApp(firebaseConfig);
+// Prevent duplicate initialization (important for Next.js)
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db };
+export { app, db };
