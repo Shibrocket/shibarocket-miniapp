89827ff7ecb29c401e813280060fc9fcbe58cefc
Shibrocket <shibarocket72@gmail.com>
2025-05-05 01:35:05 +0100

    Fix export for Firebase app

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -1,15 +1,15 @@
-// firebaseConfig.ts
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCgPHF_po0Xuq0E1hqJHK8GLnuxnDgTDbE",
   authDomain: "shibarocket-tapapp-7538a-e890c.firebaseapp.com",
   projectId: "shibarocket-tapapp-7538a-e890c",
-  storageBucket: "shibarocket-tapapp-7538a-e890c.firebasestorage.app",
+  storageBucket: "shibarocket-tapapp-7538a-e890c.appspot.com",
   messagingSenderId: "598699447123",
   appId: "1:598699447123:web:a9dc39806df1d457ce07a6",
   measurementId: "G-WCJ6BDXNSP"
 };
 
 const app = initializeApp(firebaseConfig);
-export default app;
+
+export { app }; // <--- FIXED: named export
